6fa936f74c258b653f120cb2274c9fc3937af738
Satya-Achanta <satyasri.achanta@gmail.com>
2017-03-27 16:45:31 +0530

    Adding logic to find repeated element in the array list.

---
+++ GitExample/src/main/java/com/github/maven/example/RepeatedElement.java
@@ -0,0 +1,13 @@
+package com.github.maven.example;
+
+public class RepeatedElement {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
