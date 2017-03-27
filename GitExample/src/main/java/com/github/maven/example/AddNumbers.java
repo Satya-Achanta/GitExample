ac48782736170a0c1d2685e931ccd78f3ae50fac
Satya-Achanta <satyasri.achanta@gmail.com>
2017-03-27 16:40:58 +0530

    Adding new feature to branch1

---
+++ GitExample/src/main/java/com/github/maven/example/AddNumbers.java
@@ -0,0 +1,18 @@
+package com.github.maven.example;
+
+public class AddNumbers {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 1;
+		int b = 2;
+		
+		System.out.println("Sum of numbers is :"+(a+b));
+		
+	}
+
+}
